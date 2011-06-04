5c332cea682422aa039418aa11c5901bbcaa2de9
duana <starkcoffee+apps@gmail.com>
2011-06-04 09:41:53 +0200

    rename event.title to event.summary

--- spec/lange_spec.js
+++ spec/lange_spec.js
@@ -1,16 +1,15 @@
 describe("the scheduler", function() {
     
-    it("should dates", function(){
-        var d1 = new Date("2011-01-30T17:00");
-        var d2 = new Date("2011-01-30T20:00");
-        expect(d1).toBeLessThan(d2);
-    });
+    var event1 = {summary: "one", dtstart: "2011-01-30T17:00"};
+    var event2 = {summary: "two", dtstart: "2011-01-30T20:00"};
     
     it("should order events by time", function() {
-        var event1 = {title: "one", dtstart: "2011-01-30T17:00"};
-        var event2 = {title: "two", dtstart: "2011-01-30T20:00"};
-        
         expect(Lange.schedule([event2, event1])).toEqual([event1, event2]);
+    });
+    
+    it("should handle events with no time", function() {
+        var eventWithNoDate = {summary: "foo"};
+        expect(Lange.schedule([event1, eventWithNoDate])).toEqual([event1, eventWithNoDate]);
     });
     
     it("should throw exception if not given an array of events", function(){
