9d544cdad895036a505a9a872f26882e51743273
duana <starkcoffee+apps@gmail.com>
2011-07-21 01:50:28 +0200

    minor refactoring

--- spec/ui_spec.js
+++ spec/ui_spec.js
@@ -1,32 +1,27 @@
-describe("lange-ui", function() {  
+describe("lange-ui", function() {
     var makeEventsSelectable = LangeUI.makeEventsSelectable;
     var renderPlan = LangeUI.renderPlan;
     var extractSelectedEvents = LangeUI.extractSelectedEvents;
     var numEvents;
 
-    var extractedEvent0 = {   id : 0,
-                              summary : 'See Jellyfish Play Guitar',
-                              dtstart : '2011-07-21T13:00',
-                              dateDescription : '1pm, July 21'
-                          };
-    
+
     function loadEventsFixture(){
         myLoadFixture("events.html");
         numEvents = $(".vevent").length;
         expect(numEvents).toBeGreaterThan(0);
         expect($(".vevent>input.fancy-checkbox").length).toEqual(0);
     };
-    
+
     function selectEvent(index){
         $('.fancy-checkbox').eq(index).click();
     };
-    
+
     describe("makeEventsSelectable", function(){
-        
+
         beforeEach(function(){
             loadEventsFixture();
         });
-        
+
         it("should make all events selectable", function(){
             makeEventsSelectable();
 
@@ -48,7 +43,7 @@ describe("lange-ui", function() {
 
             expect($('.vevent').first()).not.toHaveClass('selected');
         });
-        
+
         it("should call renderPlan when event is selected", function(){
             makeEventsSelectable();
             spyOn(LangeUI, 'renderPlan');
@@ -58,51 +53,57 @@ describe("lange-ui", function() {
             expect(LangeUI.renderPlan).toHaveBeenCalled();
         });
     });
-    
-    
+
+
     describe("extractSelectedEvents", function(){
-        
+
         beforeEach(function(){
             loadEventsFixture();
             makeEventsSelectable();
         });
-        
-        it("should extract a selected event including its details", function() {    
-            selectEvent(0);                 
 
-            expect(extractSelectedEvents()).toEqual([extractedEvent0]);
-        });
-        
-        it("should extract multiple selected events", function() {   
+        it("should extract a selected event including its details", function() {
             selectEvent(0);
-            selectEvent(1);                  
-            
+
+            expect(extractSelectedEvents()).toEqual([
+                {   id : 0,
+                    summary : 'See Jellyfish Play Guitar',
+                    dtstart : '2011-07-21T13:00',
+                    dateDescription : '1pm, July 21'
+                 }
+            ]);
+        });
+
+        it("should extract multiple selected events", function() {
+            selectEvent(0);
+            selectEvent(1);
+
             expect(extractSelectedEvents().length).toEqual(2);
         });
-        
-        it("should not include unselected events", function() { 
+
+        it("should not include unselected events", function() {
             selectEvent(0);
-            expect($('.vevent:not(.selected)').length).not.toEqual(0); 
-            
+            expect($('.vevent:not(.selected)').length).not.toEqual(0);
+
             expect(extractSelectedEvents().length).toEqual(1);
-        });  
-        
+        });
+
         it("should return empty array when no events selected", function(){
             expect(extractSelectedEvents()).toEqual([]);
         });
-        
-        
+
+
     });
-            
+
     describe("renderPlan", function(){
-        
+
         it("should complain if there is no div called 'plan'", function() {
             expect($('#plan')).not.toExist();
-            
+
             expect(function(){ renderPlan(); }).toThrow("expect a div called 'plan' to exist");
         });
 
-        
+
         it("should render the schedule of selected events", function() {
             spyOn(LangeUI, 'extractSelectedEvents').andReturn("selected events");
             spyOn(Lange, 'schedule').andReturn(expectedSchedule(event1,event2));
@@ -116,7 +117,7 @@ describe("lange-ui", function() {
 
             expect(Lange.schedule).toHaveBeenCalledWith("selected events");
         });
-           
+
     });
 
 
